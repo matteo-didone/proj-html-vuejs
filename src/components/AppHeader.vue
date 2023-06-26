b1083decde4d30a91d1182cdd804c65aa0915664
matteo-didone <matteo.dido9@gmail.com>
2023-06-26 16:41:32 +0200

    Added images

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,37 @@
+<template>
+    <header>
+
+
+    </header>
+</template>
+
+<script>
+import { store } from '../store.js'
+
+export default {
+    name: 'AppHeader',
+
+    components: {
+
+    },
+
+    data() {
+
+        return {
+            store,
+        };
+    },
+
+    methods: {
+
+    }
+}
+</script>
+
+
+<style lang="scss" scoped>
+    header {
+        background-color: rgba(9,20,76,255);
+
+    }
+</style>    
