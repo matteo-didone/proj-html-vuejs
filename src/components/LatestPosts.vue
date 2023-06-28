48e7bb8ff395b96790f64b5dac4aaddba98e0721
matteo-didone <matteo.dido9@gmail.com>
2023-06-28 17:48:36 +0200

    Added basic structure for the what people say

--- src/components/LatestPosts.vue
+++ src/components/LatestPosts.vue
@@ -1,5 +1,58 @@
 <template>
+    <div class="row">
+        <div class="col-md-12 title-container">
+            <span class="blog">Blog</span>
+            <h1>
+                <span class="title-bold">Latest</span> Posts
+            </h1>
+            <span class="description">
+                When, while the lovely valley teems with vapour around meand the meridian sun strikes the upper surface .
+            </span>
+        </div>
+    </div>
 
+    <div class="all-cards-container">
+        <div class="right-card">
+            <div class="big-card">
+                <div class="text-container">
+                    <p>April 21, 2019 • by Paul</p>
+                    <h2>Amazon gets CCI nod to acquire 49% stake in</h2>
+                    <p>When, while the lovely valley teems with vapour around meand the meridian sun s... </p>
+                </div>
+
+                <div class="button-container">
+                    <button class="btn">Read More</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="left-card">
+
+            <div class="smaller-card">
+                <div class="text-container">
+                    <p>April 21, 2019 • by Paul</p>
+                    <h2>Clearing Corporations get SEBI nod to invest in overnight funds</h2>
+                    <p>When, while the lovely valley teems with vapour around meand the meridian sun s... </p>
+                </div>
+
+                <div class="button-container">
+                    <button class="btn">Read More</button>
+                </div>
+            </div>
+
+            <div class="smaller-card">
+                <div class="text-container">
+                    <p>April 21, 2019 • by Paul</p>
+                    <h2>Shabnam Dhillion, wife of RSSB chief Gurinder Dhillon, passes away in London</h2>
+                    <p>When, while the lovely valley teems with vapour around meand the meridian sun s... </p>
+                </div>
+
+                <div class="button-container">
+                    <button class="btn">Read More</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -27,5 +80,26 @@ export default {
 
 
 <style lang="scss" scoped>
+.title-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 120px;
 
+    .blog {
+        color: #fa858b;
+    }
+
+    h1 {
+        margin: 10px 0;
+
+        .title-bold {
+            font-weight: bold;
+        }
+    }
+
+    .description {
+        margin-bottom: 10px;
+    }
+}
 </style>    
--- src/components/WhatPeopleSay.vue
+++ src/components/WhatPeopleSay.vue
@@ -2,22 +2,33 @@
     <div class="wrapper">
 
         <div class="title-container">
-            <p>Testimonial</p>
-            <h1> <span>What</span> People Say</h1>
-            <p>When, while the lovely valley teems with vapour around meand the meridian sun strikes the upper surface .</p>
+            <p class="testimonial">Testimonial</p>
+            <h1> <span class="title-bold">What</span> People Say</h1>
+            <p class="description">When, while the lovely valley teems with vapour around meand the meridian sun strikes the
+                upper surface .</p>
         </div>
 
-        <div class="card-container">
-            <div class="card">
-
-            </div>
-
-            <div class="card">
-
-            </div>
-
-            <div class="card">
-
+        <div class="row card-container justify-content-center">
+            <div class="card" v-for="card in cards" :key="card.title">
+                <div class="card-content">
+                    <div class="img-container">
+                        <img :src="card.image" class="card-img-top" :alt="card.alt" />
+                    </div>
+                    <div class="stars-container">
+                        <i class="fa-solid fa-star" style="color: #b6256f;"></i>
+                        <i class="fa-solid fa-star" style="color: #b6256f;"></i>
+                        <i class="fa-solid fa-star" style="color: #b6256f;"></i>
+                        <i class="fa-regular fa-star" style="color: #b6256f;"></i>
+                        <i class="fa-regular fa-star" style="color: #b6256f;"></i>
+                    </div>
+                    <div class="background-quote">
+                        "
+                    </div>
+                    <p class="card-text">When, while lovely valley teems with vapour around meand meridian sun strikes the
+                        upper impenetrable foliage of my trees, and but a thousand.</p>
+                    <h5 class="card-title">Michkel Anegl</h5>
+                    <p class="role">Marketing Management</p>
+                </div>
             </div>
         </div>
 
@@ -43,6 +54,21 @@ export default {
 
         return {
             store,
+
+            cards: [
+                {
+                    image: "https://demo.phlox.pro/corporate-2/wp-content/uploads/sites/100/2019/04/szabo-viktor-1266895-unsplash-1024x1024.png",
+                    alt: "First Card Picture",
+                },
+                {
+                    image: "https://demo.phlox.pro/corporate-2/wp-content/uploads/sites/100/2019/04/philipe-cavalcante-539505-unsplash-1024x1024.png",
+                    alt: "Second Card Picture",
+                },
+                {
+                    image: "https://demo.phlox.pro/corporate-2/wp-content/uploads/sites/100/2019/04/fabio-spinelli-695744-unsplash-1024x1024.png",
+                    alt: "Third Card Picture",
+                },
+            ],
 
             logoArray: [
                 {
@@ -77,12 +103,91 @@ export default {
 
 
 <style lang="scss" scoped>
-.logo-container{
+.logo-container {
     display: flex;
     flex-direction: row;
     justify-content: space-evenly;
     align-items: center;
     margin-top: 50px;
     margin-bottom: 50px;
-} 
+}
+
+.title-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 120px;
+
+    .testimonial {
+        color: #fa858b;
+    }
+
+    h1 {
+        margin: 10px 0;
+        font-weight: normal;
+
+        .title-bold {
+            font-weight: bolder;
+        }
+    }
+
+    .description {
+        margin-bottom: 10px;
+    }
+}
+
+.card-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 50px;
+}
+
+.card {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin: 10px;
+    width: 300px;
+    height: 400px;
+    border: none;
+}
+
+.card-content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+}
+
+.img-container {
+    width: 100px;
+    height: 100px;
+    border: 5px solid #b6256f;
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.card-text {
+    margin-top: 10px;
+}
+
+.stars-container {
+    margin-top: 10px;
+
+    .fa-star {
+        margin: 0 2px;
+    }
+}
+
+.card-title {
+    margin-top: 10px;
+}
+
+.role {
+    margin-top: 5px;
+}
 </style>    
